7f99a33121e2c9cc0d4e980aef8ea300e58a10d1
shivang shah <shivangshah2000@gmail.com>
2021-04-02 17:52:03 +0530

    Added dp loop

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(){
     for(i=1;i<=N;i++){
         cout<<points[i].first<<" "<<points[i].second<<endl;
     }
-    int slope[N+1][N+1] , intercept[N+1][N+1] , E[N+1][N+1] = {0};
+    double slope[N+1][N+1] , intercept[N+1][N+1] , E[N+1][N+1] = {0};
     int cumx[N+1], cumy[N+1] , cumxy[N+1], cumxsqr[N+1];        // cummulative arrays to store summation value
     cumx[0] = 0;
     cumy[0] = 0;
@@ -45,18 +45,32 @@ int main(){
             interval_len = j- i +1 ;
             int denominator = interval_len*xsqr_seg_sum - x_seg_sum*x_seg_sum;
             if(denominator == 0){
-                slope[i][j] = INF;            // infinity value
+                slope[i][j] = INT_MAX;            // infinity value
             }
             else{
                     slope[i][j] = (interval_len*xy_seg_sum - x_seg_sum*y_seg_sum) / (double(denominator));
             }
             intercept[i][j] = ( y_seg_sum - slope[i][j]*x_seg_sum ) / double(interval_len) ;
             for(k=i;k<=j;k++){
-                int error = points[k].second - ( slope[i][j] *points[k].first + intercept[i][j] ) ;        //  y - yi  where yi = ax +b
-                E[i][j] += pow(error,2);
+                double error = points[k].second - ( slope[i][j] *points[k].first + intercept[i][j] ) ;        //  y - yi  where yi = ax +b
+                E[i][j] += error*error;
             }
         }
     }
+
+    double OPT[N+1];
+    for(i=1;i<=N;i++){
+        OPT[i] = INT_MAX;
+    }
+    OPT[0] = 0;
+    for(j=1;j<=N;j++){
+        double temp =INT_MAX;
+        for(i=1 ;i<=j;i++){
+            temp = min(temp,E[i][j]+C+OPT[i-1]);
+        }
+        OPT[j] = temp;
+    }
+    cout<<OPT[N]<<endl;
 
    
 }
