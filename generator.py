d4ee1a1df404fa479802bf82160cff0ac2453fb7
Shreyas R <46271547+shreyas0430@users.noreply.github.com>
2021-04-08 20:05:16 +0530

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,18 @@
+import random
+
+n = int(input("No. of points: "))
+
+f = open("input.txt", "w")
+f.write("")
+f.close()
+
+f = open("input.txt", "a")
+
+for i in range(n):
+    x, y = random.randint(-45, 45), random.randint(-45, 45)
+    inpstr = str(x) + " " + str(y) + "\n"
+    f.write(inpstr)
+
+f.close()
+print("Console kya ghur rha h, input.txt dekho BC!")
+    
